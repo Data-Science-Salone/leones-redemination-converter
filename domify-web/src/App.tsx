8e7b76a44b28a365aa32c988eeea8f264491ea79
bayoh <bayohi90@gmail.com>
2022-07-01 06:32:55 +0000

    edit text

--- domify-web/src/App.tsx
+++ domify-web/src/App.tsx
@@ -71,7 +71,7 @@ function App() {
           footer={
             <Footer height={60} p="md">
             <Title order={5}>
-                Design & by the Salone Data Science Community Team
+                Design & Developed by the Salone Data Science Community Team
             </Title>
             </Footer>
           }
